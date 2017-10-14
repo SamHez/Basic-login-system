eebc3fca40ca3ea196b3504c601faa400248df56
James George <jamesgeorge998001@gmail.com>
2017-10-14 18:51:46 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+window.onload=alert("Username is your name and\npassword is your name followed by @123!");
+function validate()
+{
+  var user,pass;
+  user=document.getElementById("username").value;
+  pass=document.getElementById("password").value;
+  password=user+"@123";
+  if(user=="" || password=="")
+  {
+   alert("Both the fields are requried!");
+   return;
+  }
+  if(!(isNaN(user)))
+  {
+   alert("Username can't contain any numbers or special characters!");
+   return;
+  }
+  if(pass==password)
+  {
+   document.write("<center><h1>Successfully logged in, " + user);
+   document.write("</h1><br><br><a href='login.html'>Logout</a></center>");
+  }
+  else
+  {
+   alert("Password not matching as per the instructions.\nPlease re-enter!");
+    return;
+  }
+}
