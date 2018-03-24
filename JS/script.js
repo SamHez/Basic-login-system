0e905ea3f1b5717b5e9f0df08edb675f485ac52a
James George <jamesgeorge998001@gmail.com>
2018-03-24 10:20:51 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,22 @@
-window.onload=alert("Username is your name and\npassword is your name followed by @123!");
-function validate()
-{
-  var user,pass;
-  user=document.getElementById("username").value;
-  pass=document.getElementById("password").value;
-  password=user+"@123";
-  if(user=="" || password=="")
-  {
+window.onload = alert("Username is your name and\npassword is your name followed by @123!");
+validate: function(){
+  let user, pass;
+  user = document.getElementById("username").value;
+  pass = document.getElementById("password").value;
+  password = user + "@123";
+  if(user == "" || password == ""){
    alert("Both the fields are requried!");
    return;
   }
-  if(!(isNaN(user)))
-  {
+  if(!(isNaN(user))){
    alert("Username can't contain any numbers or special characters!");
    return;
   }
-  if(pass==password)
-  {
+  if(pass==password){
    document.write("<center><h1>Successfully logged in, " + user);
-   document.write("</h1><br><br><a href='login.html'>Logout</a></center>");
+   document.write("</h1><br><br><a href='../index.html'>Logout</a></center>");
   }
-  else
-  {
+  else{
    alert("Password not matching as per the instructions.\nPlease re-enter!");
     return;
   }
